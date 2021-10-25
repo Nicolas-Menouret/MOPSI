97dafa9cef87ea36547f3e8cd9633ff770ccb43f
Nicolas-Menouret <83075792+Nicolas-Menouret@users.noreply.github.com>
2021-10-25 18:33:04 +0200

    Update tools.h

--- tools.h
+++ tools.h
@@ -2,7 +2,10 @@
 
 #include <Imagine/Images.h>
 #include <Imagine/Graphics.h>
+#include <Imagine/LinAlg.h>
 
 Imagine::Image<Imagine::Color> LoadImage(const char* img, int&w, int&h);
 void DisplayImage(Imagine::Image<Imagine::Color> Img, Imagine::Window W, int subwin, int w, int h);
+void SelectPoints(Imagine::Image<Imagine::Color> Img1, Imagine::Image<Imagine::Color> Img2,Imagine::Window W1, Imagine::Window W2);
+Imagine::IntPoint2 Homography(Imagine::IntPoint2 x1, Imagine::Matrix<int> H);
 
