c67dce88d91f8d10904dd121fedeefb7a938b5aa
Nicolas-Menouret <83075792+Nicolas-Menouret@users.noreply.github.com>
2021-10-25 18:33:36 +0200

    Update tools.cpp

--- tools.cpp
+++ tools.cpp
@@ -26,3 +26,22 @@ void DisplayImage(Imagine::Image<Imagine::Color> Img, Imagine::Window W, int sub
     Imagine::putColorImage(0, 0, Img.data(), w, h);
 
 }
+
+Imagine::IntPoint2* SelectPoints(Imagine::Window W1, Imagine::Window W2,int nb_points){
+    Imagine::IntPoint2* coords = new Imagine::IntPoint2[2*nb_points];
+    for(int i=0;i<4;i++){
+        Imagine::setActiveWindow(W1);
+        Imagine::getMouse(coords[i]);
+        Imagine::setActiveWindow(W2);
+        Imagine::getMouse(coords[i+1]);
+    }
+    return coords;
+};
+
+Imagine::IntPoint2 Homography(Imagine::IntPoint2 p, Imagine::Matrix<int> H){
+    Imagine::IntPoint2 new_point;
+    int denum = H(2,0)*p[0]+H(2,1)*p[1]+1;
+    new_point[0]= (H(0,0)*p[0]+H(0,1)*p[1]+H(0,2))/denum;
+    new_point[1]= (H(1,0)*p[0]+H(1,1)*p[1]+H(1,2))/denum;;
+    return new_point;
+};
