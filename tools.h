2bab5d171590e5b955c47f8d840f0788df865bd4
Nicolas-Menouret <83075792+Nicolas-Menouret@users.noreply.github.com>
2021-11-22 18:47:50 +0100

    Update tools.h

--- tools.h
+++ tools.h
@@ -1,11 +1,19 @@
 #pragma once
 
+#include <Imagine/Common.h>
 #include <Imagine/Images.h>
 #include <Imagine/Graphics.h>
 #include <Imagine/LinAlg.h>
 
+
 Imagine::Image<Imagine::Color> LoadImage(const char* img, int&w, int&h);
-void DisplayImage(Imagine::Image<Imagine::Color> Img, Imagine::Window W, int subwin, int w, int h);
-void SelectPoints(Imagine::Image<Imagine::Color> Img1, Imagine::Image<Imagine::Color> Img2,Imagine::Window W1, Imagine::Window W2);
-Imagine::IntPoint2 Homography(Imagine::IntPoint2 x1, Imagine::Matrix<int> H);
+void DisplayImage(Imagine::Image<Imagine::Color> Img, Imagine::Window W, int w, int h);
+Imagine::IntPoint2* SelectPoints(Imagine::Window W1, Imagine::Window W2, int nb_points);
+Imagine::Matrix<double> FromVectorToMatrix(Imagine::FVector<double,8> h);
+Imagine::Matrix<double> FindHomography(Imagine::IntPoint2* SelectedPoints);
+Imagine::IntPoint2 Homography(Imagine::IntPoint2 x1, Imagine::Matrix<double> H);
+Imagine::IntPoint2* NewCoords(int w, int h, Imagine::Matrix<double> H);
+
+void MakeNewImage(Imagine::Image<Imagine::Color> Img1, Imagine::Image<Imagine::Color> Img2, Imagine::Matrix<double> H, int w1, int h1,int w2,int h2);
+
 
