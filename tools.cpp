d9be1498d5a991b4ac078ea71f03bc8ae94c9ecf
majidarth <arthaud.majid@gmail.com>
2022-02-04 19:30:12 +0100

    descente de gradient

--- tools.cpp
+++ tools.cpp
@@ -1,4 +1,5 @@
 #include "tools.h"
+#include <tuple>
 
 Imagine::Image<Imagine::Color> LoadImage(const char* img, int&w, int&h) {
 
@@ -132,7 +133,7 @@ int* FindExtremum(int w, int h, Imagine::IntPoint2* Coords){
     return extremum;
 }
 
-void MakeNewImage(Imagine::Image<Imagine::Color> Img1, Imagine::Image<Imagine::Color> Img2, Imagine::Matrix<double> H, int w1, int h1,int w2, int h2){
+void MakeNewImage(Imagine::Image<Imagine::Color> Img1, Imagine::Image<Imagine::Color> Img2, Imagine::Matrix<double> H, int w1, int h1, int w2, int h2){
     Imagine::IntPoint2* NewCoordsImg1 = NewCoords(w1,h1,H);
     int* extremum = FindExtremum(w1,h1,NewCoordsImg1);
     int w = std::max(w2,extremum[1])-std::min(0,extremum[0]);
@@ -154,3 +155,99 @@ void MakeNewImage(Imagine::Image<Imagine::Color> Img1, Imagine::Image<Imagine::C
     Imagine::Window W = Imagine::openWindow(w,h);
     DisplayImage(NewImage,W,w,h);
 };
+
+float Radius(int x, int y, int xc, int yc){
+    return sqrt(pow((x-xc),2) + pow((y-yc),2));
+}
+
+Imagine::Matrix<std::tuple<int, int>> DeformationMatrix(int w, int h, float k1, float k2){
+    Imagine::Matrix<std::tuple<int, int>> H(w,h);
+    for(int i=0;i<w;i++){
+        for(int j=0;j<h;j++){
+            float Factor = (1 + k1 * pow(Radius(i,j,w/2,h/2),2) + k2 * pow(Radius(i,j,w/2,h/2),4));
+            H(i,j) = std::make_tuple(int(Factor*(i-w/2) + w/2), int(Factor*(j-h/2) + h/2));
+        }
+    }
+    return H;
+}
+
+void InverseDeformation(int w, int h, int k1, int k2, Imagine::IntPoint2 Deformation_Points[4], Imagine::IntPoint2 Deformation_Cancel[4]){
+    Imagine::Matrix<std::tuple<int,int>> Deformation = DeformationMatrix(w,h,k1,k2);
+    for(int k=0;k<4;k++){
+        for(int i=0;i<w;i++){
+            for(int j=0;j<h;j++){
+                if(int(std::get<0>(Deformation(i,j))) == Deformation_Points[k][0] and int(std::get<1>(Deformation(i,j))) == Deformation_Points[k][1])
+                    Deformation_Cancel[k] = {i,j};
+            }
+        }
+    }
+}
+
+float Energy(Imagine::IntPoint2 SelectedPoints[2*4], Imagine::IntPoint2 Deformation_Cancel_1[4], int w, int h, float lambda, float mu, float k1, float k2, Imagine::Matrix<double> H){
+    Imagine::IntPoint2 Deformation_Cancel_2[4];
+    int w_homography, h_homography;
+    Imagine::IntPoint2 center_homography;
+    for(int i=0;i<4;i++)
+        Deformation_Cancel_2[i] = Homography(Deformation_Cancel_1[i], H);
+    center_homography = Homography({w, h}, H);
+    w_homography = center_homography[0];
+    h_homography = center_homography[1];
+
+    Imagine::IntPoint2 Deformation_Points_2[4];
+    for(int i=0; i<4; i++){
+        float r = Radius(Deformation_Cancel_2[i][0], Deformation_Cancel_2[i][1], w_homography/2, h_homography/2);
+        Deformation_Points_2[i] = {int((1 + k1*pow(r,2) + k2*pow(r,4))*(Deformation_Cancel_2[i][0]-w_homography) + w_homography), int((1 + k1*pow(r,2) + k2*pow(r,4))*(Deformation_Cancel_2[i][1]-h_homography) + h_homography)};
+    }
+
+    float dist = 0;
+    for(int i=0;i<4;i++)
+        dist += pow(Deformation_Points_2[i][0] -  SelectedPoints[2*i+1][0],2) + pow(Deformation_Points_2[i][1] - SelectedPoints[2*i+1][1],2);
+    return dist + lambda * pow(k1,2) + mu * pow(k2,2);
+}
+
+float EnergyDerivativeApprox(Imagine::IntPoint2 SelectedPoints[2*4], Imagine::IntPoint2 Deformation_Cancel_1[4], int w, int h, float lambda, float mu, float k1, float k2, Imagine::Matrix<double> H, float epsilon){
+    Imagine::Matrix<double> H_epsilon(3,3);
+    for(int i=0;i<3;i++){
+        for(int j=0;j<2;j++)
+            H_epsilon(i,j) = H(i,j) + epsilon;
+    }
+    return (Energy(SelectedPoints,Deformation_Cancel_1,w,h,lambda,mu,k1+epsilon,k2+epsilon,H_epsilon) - Energy(SelectedPoints,Deformation_Cancel_1,w,h,lambda,mu,k1,k2,H))/epsilon;
+}
+
+void GradientDescent(Imagine::IntPoint2 SelectedPoints[2*4], int w, int h, float lambda, float mu, int& k1, int& k2, Imagine::Matrix<double>& H, float epsilon, float accuracy){
+    int k1_prime = k1 + 1;
+    int k2_prime = k2 + 1;
+    Imagine::Matrix<double> H_prime(3,3);
+    for(int i=0;i<3;i++){
+        for(int j=0;j<2;j++)
+            H_prime(i,j) = H(i,j) + 1;
+    }
+
+    Imagine::IntPoint2 Deformation_Points_1[4] = {SelectedPoints[0], SelectedPoints[2], SelectedPoints[4], SelectedPoints[6]};
+    Imagine::IntPoint2 Deformation_Cancel_1[4];
+    InverseDeformation(w,h,k1,k2,Deformation_Points_1,Deformation_Cancel_1);
+
+    while(abs(EnergyDerivativeApprox(SelectedPoints,Deformation_Cancel_1,w,h,lambda,mu,k1,k2,H,epsilon)-EnergyDerivativeApprox(SelectedPoints,Deformation_Cancel_1,w,h,lambda,mu,k1_prime,k2_prime,H_prime,epsilon)) > accuracy){
+        float denom = EnergyDerivativeApprox(SelectedPoints,Deformation_Cancel_1,w,h,lambda,mu,k1,k2,H,epsilon)-EnergyDerivativeApprox(SelectedPoints,Deformation_Cancel_1,w,h,lambda,mu,k1_prime,k2_prime,H_prime,epsilon);
+        float facteur = -EnergyDerivativeApprox(SelectedPoints,Deformation_Cancel_1,w,h,lambda,mu,k1_prime,k2_prime,H_prime,epsilon)/denom;
+        float facteur_prime = EnergyDerivativeApprox(SelectedPoints,Deformation_Cancel_1,w,h,lambda,mu,k1,k2,H,epsilon)/denom;
+
+        float k1_mem = facteur_prime * k1_prime + facteur * k1;
+        float k2_mem = facteur_prime * k2_prime + facteur * k2;
+        k1_prime = k1;
+        k2_prime = k2;
+        k1 = k1_mem;
+        k2 = k2_mem;
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<2;j++){
+                float H_mem = facteur_prime * H_prime(i,j) + facteur * H(i,j);
+                H_prime(i,j) = H(i,j);
+                H(i,j) = H_mem;
+            }
+        }
+
+    }
+
+};
+
--- tools.h
+++ tools.h
@@ -13,7 +13,9 @@ Imagine::Matrix<double> FromVectorToMatrix(Imagine::FVector<double,8> h);
 Imagine::Matrix<double> FindHomography(Imagine::IntPoint2* SelectedPoints);
 Imagine::IntPoint2 Homography(Imagine::IntPoint2 x1, Imagine::Matrix<double> H);
 Imagine::IntPoint2* NewCoords(int w, int h, Imagine::Matrix<double> H);
-
 void MakeNewImage(Imagine::Image<Imagine::Color> Img1, Imagine::Image<Imagine::Color> Img2, Imagine::Matrix<double> H, int w1, int h1,int w2,int h2);
 
-
+void InverseDeformation(int w, int h, int k1, int k2, Imagine::IntPoint2 Deformation_Points[4], Imagine::IntPoint2 Deformation_Cancel[4]);
+Imagine::Matrix<std::tuple<int, int>> DeformationMatrix(int w, int h, float k1, float k2);
+float Radius(int x, int y, int xc, int yc);
+float Energy(float k1, float k2, Imagine::Matrix<double> H);
