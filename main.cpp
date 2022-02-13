10e587b671a0204df26729c25487e2a409bd6bf1
Nicolas-Menouret <83075792+Nicolas-Menouret@users.noreply.github.com>
2022-02-13 17:22:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@ const char* default_color_image_file2=srcPath("/right.png");
 
 int main() {
 
+
     int w1,h1,w2,h2;
     Imagine::Image<Imagine::Color> Img1 = LoadImage(default_color_image_file1, w1, h1);
     Imagine::Image<Imagine::Color> Img2 = LoadImage(default_color_image_file2, w2, h2);
@@ -27,10 +28,23 @@ int main() {
     DisplayImage(Img2,W2,w2,h2);
 
     IntPoint2* Selection = SelectPoints(W1,W2);
+    //Version without deformation
+    /*
     Matrix<double> H = FindHomography(Selection);
 
     MakeNewImage(Img1,Img2,H,w1,h1,w2,h2);
-    endGraphics();
+    endGraphics();*/
 
+
+    // Version with deformation
+    Matrix<double> H(3,3);
+    int k1,k2;
+    GradientDescent(Selection, w1, h1, k1, k2, H);
+    MakeNewImage2(Img1,Img2,H,w1,h1,w2,h2,k1,k2);
     return 0;
 }
+
+
+
+
+
