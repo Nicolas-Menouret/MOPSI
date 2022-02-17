3647cd3bebd9e714ab611082114f8365edba0ca6
Nicolas-Menouret <83075792+Nicolas-Menouret@users.noreply.github.com>
2022-02-17 12:42:44 +0100

    Update tools.h

--- tools.h
+++ tools.h
@@ -8,19 +8,20 @@
 
 Imagine::Image<Imagine::Color> LoadImage(const char* img, int&w, int&h);
 void DisplayImage(Imagine::Image<Imagine::Color> Img, Imagine::Window W, int w, int h);
-Imagine::IntPoint2* SelectPoints(Imagine::Window W1, Imagine::Window W2, int n=4);
+Imagine::IntPoint2* SelectPoints(Imagine::Window W1, Imagine::Window W2, int n_points=4);
 Imagine::Matrix<double> FromVectorToMatrix(Imagine::FVector<double,8> h);
 Imagine::Matrix<double> FindHomography(Imagine::IntPoint2* SelectedPoints);
 Imagine::IntPoint2 Homography(Imagine::IntPoint2 x1, Imagine::Matrix<double> H);
 Imagine::IntPoint2* NewCoords(int w, int h, Imagine::Matrix<double> H);
 void MakeNewImage(Imagine::Image<Imagine::Color> Img1, Imagine::Image<Imagine::Color> Img2, Imagine::Matrix<double> H, int w1, int h1,int w2,int h2);
 
-float Radius(int x, int y, int xc, int yc);
-float Energy(Imagine::IntPoint2 SelectedPoints[2*4], Imagine::IntPoint2 Deformation_Cancel_1[4], int w, int h, float lambda, float mu, float k1, float k2, Imagine::Matrix<double> H);
-void GradientDescent(Imagine::IntPoint2* SelectedPoints, int w, int h,  int& k1, int& k2, Imagine::Matrix<double>& H,float lambda=0.1, float mu=0.1, float epsilon=0.1, int n_iterations=100, float speed=0.1);
+double Radius(int x, int y, int xc, int yc);
+double Energy(Imagine::IntPoint2* SelectedPoints, Imagine::IntPoint2* Deformation_Cancel_1, int w, int h, double lambda, double mu, double k1, double k2, Imagine::Matrix<double> H, int n_points = 4);
+void GradientDescent(Imagine::IntPoint2* SelectedPoints, int w, int h,  double& k1, double& k2, Imagine::Matrix<double>& H,int n_points = 4, double lambda=0.1, double mu=0.1, double epsilon=pow(10,-1), int n_iterations=1000, double speed=pow(10,-1));
 
-void MakeNewImage2(Imagine::Image<Imagine::Color> Img1, Imagine::Image<Imagine::Color> Img2, Imagine::Matrix<double> H, int w1, int h1, int w2, int h2, int k1, int k2);
-Imagine::FMatrix<float,2,1>  Deformation(Imagine::FMatrix<float,2,1>   p, int w, int h,float k1, float k2);
-Imagine::FMatrix<float,1,2> Transpose(Imagine::FMatrix<float,2,1> M);
-Imagine::IntPoint2* NoDeformationImage(int w, int h, float k1, float k2);
-Imagine::IntPoint2 InverseDeformationQuasiNewton(Imagine::IntPoint2 p, float k1, float k2, int w, int h,float epsilon = 0.01);
+void MakeNewImage2(Imagine::Image<Imagine::Color> Img1, Imagine::Image<Imagine::Color> Img2, Imagine::Matrix<double> H, int w1, int h1, int w2, int h2, double k1, double k2);
+Imagine::FMatrix<double,2,1>  Deformation(Imagine::FMatrix<double,2,1>   p, int w, int h,double k1, double k2);
+Imagine::FMatrix<double,1,2> Transpose(Imagine::FMatrix<double,2,1> M);
+Imagine::IntPoint2* NoDeformationImage(int w, int h, double k1, double k2);
+Imagine::IntPoint2 InverseDeformationQuasiNewton(Imagine::IntPoint2 p, double k1, double k2, int w, int h,double epsilon = 0.01);
+
